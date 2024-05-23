34e2c0cc073a0b35198a38349712103d286fa3fe
Abubakar cadet <abbakar.cyber@gmail.com>
2024-05-23 14:36:16 +0100

    added some buttons

--- components/LastMinuteDeals.tsx
+++ components/LastMinuteDeals.tsx
@@ -10,6 +10,7 @@ const LastMinuteDeals = () => {
               a limited number of these deals are available but they represent our absolute lowest price for either our 
               1 hour Massage of Facial.
             </p>
+            <button className='block bg-gray-400  px-16 py-2 rounded mt-6'>FIND A LAST MINUTE DEAL</button>
         </div>
     </div>
   );
---
+++ components/NewService.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NewService = () => {
+  return (
+    <div>Request Service</div>
+  )
+}
+
+export default NewService
