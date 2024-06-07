08ce39e19096ce963ad2293f2c3a587ef1e564ce
Abubakar cadet <abbakar.cyber@gmail.com>
2024-06-07 17:40:17 +0100

    added conditions

--- components/Terms.tsx
+++ components/Terms.tsx
@@ -1,16 +1,14 @@
 const Terms = () => {
     return (
       <div className="max-w-xl mx-auto my-10 p-6 bg-white rounded-lg shadow-lg">
-        <h2 className="text-2xl font-bold mb-4">Terms and Conditions</h2>
+        <h2 className="text-2xl font-bold mb-4 bold-40">Terms and Conditions</h2>
         <p className="text-gray-700 mb-4">
           Welcome to our application. Please read these terms and conditions carefully
           before using our services.
         </p>
-        <p>DEPOSITS
-        APPOINTMENTS
-        CANCELLATIONS
-        REFUND A non-refundable
-        You must reschedule You must cancel 24
+        <p><strong className="bold-32 ">DEPOSITS APPOINTMENTS CANCELLATIONS REFUND:</strong>
+            A non-refundable,
+        You must reschedule,
         There are no deposit of $25 is 24 hours in advanced. hours in advance to refunds once
         required to book. If you are running transfer your deposit service is rendered. This deposit will go
         late, there is a 10 to another If for some reason towards your final minute grace period. appointment.
@@ -19,6 +17,17 @@ const Terms = () => {
         72 hours to reach are allowed one appointment will be their set. Card out to me and come deposit transfer to
         cancelled. declining will result in get them fixed at a new appointment No call No shows must being banned from
         the earliest when rescheduling.pay 100% of set price booking unless you pay connivence.
+        </p>
+        <p><strong className="font-bold text-2xl ">BEFORE YOU VISIT</strong>
+                No public restroom! Please use the bathroom prior to your appointment.
+                No kids or plus ones! Space is limited.
+                Please silence your phone or set on Do Not Disturb prior to service.
+                Cell phones are a distraction, please bring Air Pods for important calls.
+                Please send me a text or DM on instagram/facebook to let me know when you have arrived. I will let you know when I am ready for you!
+                Arrive with no make-up and clean lashes please! The cleaner your lashes are the better the extensions will stick to your natural lashes.
+                Squeeze in appointments are an additional $25.
+                My after hour fee is $25 between the hours of 7am-6pm.
+                I do accept foreign fills! My foreign fill fee is $25.
         </p>
       </div>
     );
