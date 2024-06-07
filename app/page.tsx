0fab08bfb3bf4ccd34be13fa1481acc546ff28fb
Abubakar cadet <abbakar.cyber@gmail.com>
2024-06-07 16:09:12 +0100

    added terms but needed to edit it

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,14 @@ import LastMinuteDeals from "@/components/LastMinuteDeals";
 import Offers from "@/components/Offers";
 import NewService from "@/components/NewService";
 import VideoPlayer from "@/components/VideoPlayer"
+import Terms from "@/components/Terms";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Camp />
+      <Terms />
       <Guide />
       <VideoPlayer />
       <Features />
---
+++ components/Terms.tsx
@@ -0,0 +1,27 @@
+const Terms = () => {
+    return (
+      <div className="max-w-xl mx-auto my-10 p-6 bg-white rounded-lg shadow-lg">
+        <h2 className="text-2xl font-bold mb-4">Terms and Conditions</h2>
+        <p className="text-gray-700 mb-4">
+          Welcome to our application. Please read these terms and conditions carefully
+          before using our services.
+        </p>
+        <p>DEPOSITS
+        APPOINTMENTS
+        CANCELLATIONS
+        REFUND A non-refundable
+        You must reschedule You must cancel 24
+        There are no deposit of $25 is 24 hours in advanced. hours in advance to refunds once
+        required to book. If you are running transfer your deposit service is rendered. This deposit will go
+        late, there is a 10 to another If for some reason towards your final minute grace period. appointment.
+        you have any payment due on the After 10 minutes is a Cancellation notice problems with your day of your
+        $15 fee. By 15 minute less than 24 hours will lashes, you will have appointment. You mark, your be charged 50% of
+        72 hours to reach are allowed one appointment will be their set. Card out to me and come deposit transfer to
+        cancelled. declining will result in get them fixed at a new appointment No call No shows must being banned from
+        the earliest when rescheduling.pay 100% of set price booking unless you pay connivence.
+        </p>
+      </div>
+    );
+  };
+  
+  export default Terms;
